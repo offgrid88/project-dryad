90876904f424119cb9143b5f2b5e1008c166470e
Aymen Rachdi <45432048+offgrid88@users.noreply.github.com>
2024-08-08 10:17:14 +0100

    Update oneshot_read_main.c

--- main/oneshot_read_main.c
+++ main/oneshot_read_main.c
@@ -1,202 +1,62 @@
-/*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "soc/soc_caps.h"
-#include "esp_log.h"
-#include "esp_adc/adc_oneshot.h"
-#include "esp_adc/adc_cali.h"
-#include "esp_adc/adc_cali_scheme.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "mqtt_client.h"
 
-const static char *TAG_DRYAD_ADC = "DRYAD_ADC";
+#define DEFAULT_VREF    1100
+#define NO_OF_SAMPLES   64
 
-/*---------------------------------------------------------------
-        ADC General Macros
----------------------------------------------------------------*/
-//ADC1 Channels
-#if CONFIG_IDF_TARGET_ESP32
-#define DRYAD_ADC1_CHAN0          ADC_CHANNEL_0
-#define DRYAD_ADC1_CHAN1          ADC_CHANNEL_5
-#else
-#define DRYAD_ADC1_CHAN0          ADC_CHANNEL_2
-#define DRYAD_ADC1_CHAN1          ADC_CHANNEL_3
-#endif
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;  // GPIO34 if ADC1
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_unit_t unit = ADC_UNIT_1;
 
-#if (SOC_ADC_PERIPH_NUM >= 2) && !CONFIG_IDF_TARGET_ESP32C3
-/**
- * On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
- * Search for errata on espressif website for more details.
- */
-#define DRYAD_USE_ADC2            1
-#endif
+static const char *MQTT_URI = "mqtt://mqtt.aymenrachdi.xyz";
+static esp_mqtt_client_handle_t mqtt_client = NULL;
 
-#if DRYAD_USE_ADC2
-//ADC2 Channels
-#if CONFIG_IDF_TARGET_ESP32
-#define DRYAD_ADC2_CHAN0          ADC_CHANNEL_0
-#else
-#define DRYAD_ADC2_CHAN0          ADC_CHANNEL_0
-#endif
-#endif  //#if DRYAD_USE_ADC2
+// Define the dry (0% moisture) and wet (100% moisture) calibration values
+const uint32_t DRY_VALUE = 0;    // adjust this value
+const uint32_t WET_VALUE = 3000; // adjust this value
 
-#define DRYAD_ADC_ATTEN           ADC_ATTEN_DB_12
-
-static int adc_raw[2][10];
-static int voltage[2][10];
-static bool dryad_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
-static void dryad_adc_calibration_deinit(adc_cali_handle_t handle);
-
-void app_main(void)
-{
-    //-------------ADC1 Init---------------//
-    adc_oneshot_unit_handle_t adc1_handle;
-    adc_oneshot_unit_init_cfg_t init_config1 = {
-        .unit_id = ADC_UNIT_1,
+void mqtt_app_start(void) {
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = MQTT_URI,
     };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+    mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(mqtt_client);
+}
 
-    //-------------ADC1 Config---------------//
-    adc_oneshot_chan_cfg_t config = {
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-        .atten = DRYAD_ADC_ATTEN,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, DRYAD_ADC1_CHAN0, &config));
-    //ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, DRYAD_ADC1_CHAN1, &config));
+void send_mqtt_message(uint32_t moisture_percent) {
+    char payload[100];
+    sprintf(payload, "Moisture Level: %d%%", moisture_percent);
+    esp_mqtt_client_publish(mqtt_client, "soil/moisture", payload, 0, 1, 0);
+}
 
-    //-------------ADC1 Calibration Init---------------//
-    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
-    adc_cali_handle_t adc1_cali_chan1_handle = NULL;
-    bool do_calibration1_chan0 = dryad_adc_calibration_init(ADC_UNIT_1, DRYAD_ADC1_CHAN0, DRYAD_ADC_ATTEN, &adc1_cali_chan0_handle);
-    bool do_calibration1_chan1 = dryad_adc_calibration_init(ADC_UNIT_1, DRYAD_ADC1_CHAN1, DRYAD_ADC_ATTEN, &adc1_cali_chan1_handle);
-
-#if DRYAD_USE_ADC2
-    //-------------ADC2 Init---------------//
-    adc_oneshot_unit_handle_t adc2_handle;
-    adc_oneshot_unit_init_cfg_t init_config2 = {
-        .unit_id = ADC_UNIT_2,
-        .ulp_mode = ADC_ULP_MODE_DISABLE,
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &adc2_handle));
-
-    //-------------ADC2 Calibration Init---------------//
-    adc_cali_handle_t adc2_cali_handle = NULL;
-    bool do_calibration2 = dryad_adc_calibration_init(ADC_UNIT_2, DRYAD_ADC2_CHAN0, DRYAD_ADC_ATTEN, &adc2_cali_handle);
-
-    //-------------ADC2 Config---------------//
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, DRYAD_ADC2_CHAN0, &config));
-#endif  //#if DRYAD_USE_ADC2
+void app_main() {
+    // ADC configuration
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(channel, atten);
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+    mqtt_app_start();
 
     while (1) {
-        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, DRYAD_ADC1_CHAN0, &adc_raw[0][0]));
-        ESP_LOGI(TAG_DRYAD_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, DRYAD_ADC1_CHAN0, adc_raw[0][0]);
-        if (do_calibration1_chan0) {
-            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_raw[0][0], &voltage[0][0]));
-            ESP_LOGI(TAG_DRYAD_ADC, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, DRYAD_ADC1_CHAN0, voltage[0][0]);
+        uint32_t adc_reading = 0;
+        for (int i = 0; i < NO_OF_SAMPLES; i++) {
+            adc_reading += adc1_get_raw((adc1_channel_t)channel);
         }
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        adc_reading /= NO_OF_SAMPLES;
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
 
-        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, DRYAD_ADC1_CHAN1, &adc_raw[0][1]));
-        //ESP_LOGI(TAG_DRYAD_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, DRYAD_ADC1_CHAN1, adc_raw[0][1]);
-        if (do_calibration1_chan1) {
-            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan1_handle, adc_raw[0][1], &voltage[0][1]));
-            //ESP_LOGI(TAG_DRYAD_ADC, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, DRYAD_ADC1_CHAN1, voltage[0][1]);
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        // Calculate moisture as a percentage
+        uint32_t moisture_percent = 100 * (voltage - DRY_VALUE) / (WET_VALUE - DRY_VALUE);
+        moisture_percent = (moisture_percent > 100) ? 100 : (moisture_percent < 0) ? 0 : moisture_percent;
 
-#if DRYAD_USE_ADC2
-        ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, DRYAD_ADC2_CHAN0, &adc_raw[1][0]));
-        //ESP_LOGI(TAG_DRYAD_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_2 + 1, DRYAD_ADC2_CHAN0, adc_raw[1][0]);
-        if (do_calibration2) {
-            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc2_cali_handle, adc_raw[1][0], &voltage[1][0]));
-            //ESP_LOGI(TAG_DRYAD_ADC, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_2 + 1, DRYAD_ADC2_CHAN0, voltage[1][0]);
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));
-#endif  //#if DRYAD_USE_ADC2
+        printf("Voltage: %dmV, Moisture Level: %d%%\n", voltage, moisture_percent);
+        send_mqtt_message(moisture_percent);
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
-
-    //Tear Down
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
-    if (do_calibration1_chan0) {
-        dryad_adc_calibration_deinit(adc1_cali_chan0_handle);
-    }
-    if (do_calibration1_chan1) {
-        dryad_adc_calibration_deinit(adc1_cali_chan1_handle);
-    }
-
-#if DRYAD_USE_ADC2
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc2_handle));
-    if (do_calibration2) {
-        dryad_adc_calibration_deinit(adc2_cali_handle);
-    }
-#endif //#if DRYAD_USE_ADC2
-}
-
-/*---------------------------------------------------------------
-        ADC Calibration
----------------------------------------------------------------*/
-static bool dryad_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
-{
-    adc_cali_handle_t handle = NULL;
-    esp_err_t ret = ESP_FAIL;
-    bool calibrated = false;
-
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    if (!calibrated) {
-        ESP_LOGI(TAG_DRYAD_ADC, "calibration scheme version is %s", "Curve Fitting");
-        adc_cali_curve_fitting_config_t cali_config = {
-            .unit_id = unit,
-            .chan = channel,
-            .atten = atten,
-            .bitwidth = ADC_BITWIDTH_DEFAULT,
-        };
-        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
-        if (ret == ESP_OK) {
-            calibrated = true;
-        }
-    }
-#endif
-
-#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    if (!calibrated) {
-        ESP_LOGI(TAG_DRYAD_ADC, "calibration scheme version is %s", "Line Fitting");
-        adc_cali_line_fitting_config_t cali_config = {
-            .unit_id = unit,
-            .atten = atten,
-            .bitwidth = ADC_BITWIDTH_DEFAULT,
-        };
-        ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
-        if (ret == ESP_OK) {
-            calibrated = true;
-        }
-    }
-#endif
-
-    *out_handle = handle;
-    if (ret == ESP_OK) {
-        ESP_LOGI(TAG_DRYAD_ADC, "Calibration Success");
-    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-        ESP_LOGW(TAG_DRYAD_ADC, "eFuse not burnt, skip software calibration");
-    } else {
-        ESP_LOGE(TAG_DRYAD_ADC, "Invalid arg or no memory");
-    }
-
-    return calibrated;
-}
-
-static void dryad_adc_calibration_deinit(adc_cali_handle_t handle)
-{
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    ESP_LOGI(TAG_DRYAD_ADC, "deregister %s calibration scheme", "Curve Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
-
-#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    ESP_LOGI(TAG_DRYAD_ADC, "deregister %s calibration scheme", "Line Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
-#endif
 }
